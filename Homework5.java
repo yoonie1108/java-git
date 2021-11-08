c841a8b2a578e70b6c833d74d9eb9c8cac89de10
yoonie1108 <sky04058@naver.com>
2021-11-09 04:47:12 +0900

    Add Java File [Homework5.java]

---
+++ Homework5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PersonalComputer {
+    private Monitor monitor;
+    private Computer computer;
+    private String monitorSize;
+    private String color;
+    private String power;
+    private String memory;
+    private String hd;
+    private String cpu;
+    Computer computer = new Computer;
+    Monitor monitor = new Monitor;
+
+  PersonalComputer(Computer computer,Monitor monitor){
+       this.computer=computer;
+       this.monitor=monitor;
+   }
+
+    public void turnOn();
+
+}
+
+public class Homework5{
+}

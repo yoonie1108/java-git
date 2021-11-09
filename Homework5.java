2d081bb7ba30f90014ccf493d5a9cb7d5c76ac3a
yoonie1108 <sky04058@naver.com>
2021-11-10 03:12:05 +0900

    Add Java File [Homework5.java]

--- Homework5.java
+++ Homework5.java
@@ -1,24 +1,60 @@
-import java.util.Scanner;
-public class PersonalComputer {
-    private Monitor monitor;
-    private Computer computer;
-    private String monitorSize;
-    private String color;
-    private String power;
-    private String memory;
-    private String hd;
-    private String cpu;
-    Computer computer = new Computer;
-    Monitor monitor = new Monitor;
-
-  PersonalComputer(Computer computer,Monitor monitor){
-       this.computer=computer;
-       this.monitor=monitor;
-   }
-
-    public void turnOn();
-
-}
-
+import java.turnon.PersonalComputer
 public class Homework5{
+
+    class PersonalComputer {
+        private Computer computer;
+        private Monitor monitor;
+        PersonalComputer(Computer computer,Monitor monitor){
+            this.computer =computer;
+            this.monitor=monitor;
+        }
+        public String toString(){
+            return System.out.println("Turning on the computer");
+            System.out.println("Turning on the monitor");
+        }
+    }
+    class Monitor {
+        private Computer computer;
+        private Monitor monitor;
+        PersonalComputer(Computer computer, Monitor monitor){
+            this.computer =computer;
+            this.monitor=monitor;
+        }
+        public String toString(){
+            return System.out.println("Turning on the computer");
+            System.out.println("Turning on the monitor");
+        }
+    }
+    public class Computer {
+        private String cpu;
+        private String memory;
+        private String hd;
+        private String color;
+        private String power;
+
+        public String getCpu(){
+            return cpu;
+        }
+        public String getMemory(){
+            return memory;
+        }
+        public String getHd(){
+            return hd;
+        }
+        public String getColor(){
+            return color;
+        }
+        public String getPower(){
+            return power;
+        }
+        public String toString(){
+            return "Cpu"+cpu.toString()+"\nMemory;"+memory.toString()+"\nHDD"+hd.toString()+"\nColor"+color.toString()+"\npower"+power.toString();
+        }
+    }
+    public static void main(String[] args){
+        PersonalComputer printInfo = new PersonalComputer(
+                new Monitor("24 inch"+"Black"+"45w");
+        new Computer("Core i7"+"16GB"+"2TB"+"White"+"500W"));
+        System.out.println(printInfo);
+    }
 }
